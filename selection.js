5b81089113656538f95f5c10b9a0c47cf7ec43f0
teichmath <teichmath@gmail.com>
2016-02-17 22:00:35 -0600

    Update selection.js

--- selection.js
+++ selection.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
-	$('#dragon1').click($.proxy(selectionApp.changeSelectionStatus));
+	$('#dragonA').click($.proxy(selectionApp.changeSelectionStatus));
 });
 
 
 var selectionApp = (function (){
 	//private variables and functions (hidden props)
- 	var dragon1status = false;
+ 	var dragonAstatus = false;
 
 	return {
 		//public props
 	
 		changeSelectionStatus: function(){
-		  dragon1status = !dragon1status;
-  		  var imgTag=document.getElementsByTagName('dragon1');
-		  if(dragon1status) imgTag.style.border='2px solid #F0E68C';
+		  dragonAstatus = !dragonAstatus;
+  		  var imgTag=document.getElementsByTagName('dragonA');
+		  if(dragonAstatus) imgTag.style.border='2px solid #F0E68C';
 		  else imgTag.style.border = 'none';
 		}
 	};
